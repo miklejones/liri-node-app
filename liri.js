dde33334cf9570493138a98e4967fcb2e2f6db69
Michael Garland <michaelcgarland@gmail.com>
2018-04-18 09:40:14 -0400

    omdb functioning

--- liri.js
+++ liri.js
@@ -2,7 +2,10 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var request = require("request");
 var keys = require("./keys.js");
+var fs = require("fs");
+
 
 //set keys
 var spotify = new Spotify(keys.spotify);
@@ -10,13 +13,10 @@ var client = new Twitter(keys.twitter);
 
 // Take in the command line arguments
 var nodeArgs = process.argv;
-
 var task = nodeArgs[2];
 var userRequest = nodeArgs[3];
 
-
 //Set up Functions for use in logic
-
 function myTweets() {
     var params = {
         q: 'Mikle_Jones',
@@ -25,8 +25,6 @@ function myTweets() {
     };
     client.get('search/tweets', params, function (error, tweets, response) {
         if (!error) {
-            //need an if else statement for if there is a search term
-
             //for loop that pulls just the tweet and when they were created
             for (let i = 0; i < tweets.statuses.length; i++) {
                 let time = tweets.statuses[i].created_at;
@@ -39,8 +37,6 @@ function myTweets() {
 };
 
 function spotifyThisSong() {
-
-    
     //if statement that will return info for ace of base if nothing is put in
     if (!userRequest) {
         spotify.search({ type: 'track', query: "The Sign Ace of Base", limit: 1 }, function (err, data) {
@@ -50,14 +46,11 @@ function spotifyThisSong() {
             let albumName = data.tracks.items[0].album.name;
             let songName = data.tracks.items[0].name;
             let artist = data.tracks.items[0].artists[0].name;
-
             console.log("Artist : " + artist);
             console.log("Song : " + songName);
             console.log("Album : " + albumName);
         });
     } else {
-
-
         spotify.search({ type: 'track', query: userRequest, limit: 1 }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
@@ -65,14 +58,33 @@ function spotifyThisSong() {
             let albumName = data.tracks.items[0].album.name;
             let songName = data.tracks.items[0].name;
             let artist = data.tracks.items[0].artists[0].name;
-
             console.log("Artist : " + artist);
             console.log("Song : " + songName);
             console.log("Album : " + albumName);
-
         });
     };
-    // search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+};
+
+function movieThis() {
+    request(`http://www.omdbapi.com/?t=${userRequest}&apikey=trilogy`, function (error, response, body) {
+        // If there were no errors and the response code was 200 (i.e. the request was successful)...
+        if (!error && response.statusCode === 200) {
+            // Then we print out the imdbRating
+            var parsedBody = JSON.parse(body);
+            console.log("Title: " + parsedBody.Title);
+            console.log("Year: " + parsedBody.Year);
+            console.log("imdb Rating: " + parsedBody.imdbRating);
+            console.log("Rotten Tomatoe's rating: " + parsedBody.Ratings[1].Value);
+            console.log("Country made in: " + parsedBody.Country);
+            console.log("Launguage: " + parsedBody.Language);
+            console.log("Plot :" + parsedBody.Plot);
+            console.log("Actors :" + parsedBody.Actors);
+        }
+    });
+}
+
+function doWhatItSays() {
+
 }
 
 //set up logic for functions to play in
@@ -83,12 +95,12 @@ switch (task) {
     case 'spotify-this-song':
         spotifyThisSong()
         break;
-    // case 'movie-this':
-    //     code block
-    //     break;
+    case 'movie-this':
+        movieThis()
+        break;
     // case 'do-what-it-says':
-    //     code block
+    //     doWhatItSays()
     //     break;
-    // default:
-    //     code block
+    default:
+        console.log('you a asshole');
 }
